6de60adb06101e120c04b65d4ba657414c0a9371
lovelock <happyhackerwqc@foxmail.com>
2021-01-18 12:13:45 +0800

    updating content Mon Jan 18 12:13:45 CST 2021

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 hugo -t maupassant
 
-node search/lunr/bin/cli.js
+node search/lunr/bin/cli.js -s false
 
 # Go To Public folder
 cd public
