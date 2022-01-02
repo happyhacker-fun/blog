8ec35733056d929a727dedba190762a5f590ae02
Frost Wong <happyhackerwqc@foxmail.com>
2022-01-02 16:35:44 +0800

    更换到github-style主题

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t maupassant
-
-node search/lunr/bin/cli.js
+hugo -t github-style
 
 # Go To Public folder
 cd public
